45de02f89310691396f18d68f233642c6258c188
Mohamed Riabi <mhdriabi2013@gmail.com>
2023-01-13 20:16:55 +0100

    getTokenSecret method to get the tokenSecret wich is inside properties file

--- src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.app.ws.security;
 
+import com.appsdeveloperblog.app.ws.SpringApplicationContext;
+
 public class SecurityConstants{
 
     public static final long EXPIRATION_TIME = 864000000; //10DAYS
@@ -7,5 +9,9 @@ public class SecurityConstants{
     public static final String TOKEN_PREFIX = "Bearer";
     public static final  String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL ="/users";
-    public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";
+
+    public static String getTockenSecret(){
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+        return appProperties.getTockenString();
+    }
 }
